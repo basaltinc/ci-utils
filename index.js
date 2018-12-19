d33c4e3ce8af776bf1b692f0d550c0aeb6701a21
Evan Lovely <evanlovely@gmail.com>
2018-12-19 15:39:07 -0800

    feat: setting up gh-status

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 #!/usr/bin/env node
 const program = require('commander');
-// const fetch = require('node-fetch');
-// const { execSync } = require('child_process');
+const fetch = require('node-fetch');
+const { execSync } = require('child_process');
 const { version } = require('./package.json');
 
-const { GITHUB_TOKEN } = process.env;
+let repoSlug;
+const { GITHUB_TOKEN, TRAVIS } = process.env;
 
 if (!GITHUB_TOKEN) {
   console.log('Missing env var: "GITHUB_TOKEN"');
   process.exit(1);
+}
+
+if (TRAVIS) {
+  const { TRAVIS_REPO_SLUG } = process.env;
+  repoSlug = TRAVIS_REPO_SLUG;
+}
+
+if (!repoSlug) {
+  console.log('Missing repoSlug');
+  process.exit(1);
+}
+
+/**
+ * @param {string} cmd
+ * @returns {string} - stdout results
+ */
+function sh(cmd) {
+  try {
+    return execSync(cmd, {
+      encoding: 'utf8',
+    }).trim();
+  } catch (error) {
+    console.log(`Uh oh. Error running "${cmd}"`, error);
+    process.exit(1);
+  }
 }
 
 program.version(version, '-v, --version');
@@ -25,8 +51,26 @@ program
   .option('-c, --context [context]', 'Context')
   .option('-d, --description [description]', 'Description')
   .action(cmd => {
+    const gitShaLong = sh('git rev-parse HEAD');
     const { state, url, context, description } = cmd;
-    console.log('gh-status', { state, url, context, description });
+    // console.log('gh-status', { state, url, context, description, gitShaLong });
+    return fetch(
+      `https://api.github.com/repos/${repoSlug}/statuses/${gitShaLong}`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          state,
+          target_url: url,
+          context,
+          description,
+        }),
+        headers: {
+          Authorization: `Bearer ${GITHUB_TOKEN}`,
+        },
+      },
+    )
+      .then(res => res.json())
+      .then(results => console.log(results));
   });
 
 program.parse(process.argv);
