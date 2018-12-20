6d0802ae623443488b506e62e9faaaae2f415135
Evan Lovely <evanlovely@gmail.com>
2018-12-20 14:22:59 -0800

    feat: add github-comment util

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const program = require('commander');
-const { setGitHubStatus } = require('./lib');
+const { setGitHubStatus, createGitHubComment } = require('./lib');
 const { version } = require('./package.json');
 
 program.version(version, '-v, --version');
@@ -25,6 +25,18 @@ program
       description,
     }).then(results => {
       console.log(`GitHub status set to: ${results.state}`);
+      return true;
+    });
+  });
+
+program
+  .command('gh-comment')
+  .option('-c, --comment [comment]', 'Comment; markdown ok')
+  .option('-i, --issue [issue]', 'GitHub issue/PR id number', parseInt)
+  .action(cmd => {
+    const { comment, issue } = cmd;
+    return createGitHubComment(comment, issue).then(results => {
+      console.log(`GitHub comment made: ${results.html_url}`);
       return true;
     });
   });
